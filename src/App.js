539500c654def23e893fc57668a55a6b22678740
Rohit-24gb <rohitrajaksnd@gmail.com>
2024-01-03 23:29:23 +0530

    The (generated by blackbox.ai )

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import styled, { ThemeProvider } from 'styled-components';
+import { darkTheme } from './utlis/Themes';
+import Navbar from './components/Navbar';
+import Hero from './components/HeroSection';
+import Skill from './components/SkillSection';
+import Education from './components/Education';
+import Experience from './components/Experience';
+// import { Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ImportExport } from '@mui/icons-material';
+import Projects from './components/Projects';
+
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width :100%;
+  overflow-x : hidden;
+`;
+
+const Wrapper = styled.div`
+  background: linear-gradient(
+    38.73deg,
+    rgba(204, 0 ,187, 0.15) 0%,
+    rgba(201, 32 ,184, 0) 50%,
+  ),
+  linear-gradient(
+    141.27deg,
+    rgba(0, 70 ,289, 0) 50%,
+    rgba(0, 70 ,289, 0.15) 100%,
+  );
+  width: 100%;
+  clip-path: polygon(0 0,100% 0, 100% 100%, 0 calc(100% -5vw));
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Router>
+        <Navbar />
+        <Body>
+          <Hero />
+          <Wrapper>
+            <Skill />
+            <Experience />
+            <Education />
+          </Wrapper>
+          <Projects />
+        </Body>
+      </Router>
+    </ThemeProvider>
   );
 }
 
